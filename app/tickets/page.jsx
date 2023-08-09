f17c9181604b4cd630ad5d1fe846fabfb8c1b0ec
Michał Januszko <michal@januszko.net>
2023-08-09 21:34:29 +0200

    Added suspense boundary for the ticket list

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,4 +1,6 @@
 import TicketList from './TicketList';
+import {Suspense} from "react";
+import Loading from "@/app/loading";
 
 function Tickets() {
   return (
@@ -11,7 +13,9 @@ function Tickets() {
           </p>
         </div>
       </nav>
-      <TicketList />
+        <Suspense fallback={<Loading />}>
+            <TicketList />
+        </Suspense>
     </main>
   );
 }
