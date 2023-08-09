4d5c41c0b1ca3a54f0cdbcee212fe037ba30bc06
Michał Januszko <michal@januszko.net>
2023-08-09 21:21:48 +0200

    Added custom 404 page

---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function NotFound(props) {
+    return (
+        <main className='text-center'>
+            <h2 className='text-3xl'>There was a problem.</h2>
+            <p>We could not find the ticket you were looking for.</p>
+            <p>Go back to <Link href='/tickets/'>all tickets</Link></p>
+        </main>
+    );
+}
+
+export default NotFound;
