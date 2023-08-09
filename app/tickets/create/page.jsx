bd801b71b4b8e3e180793a781a262ba5566a9468
Michał Januszko <michal@januszko.net>
2023-08-09 22:06:57 +0200

    Added Create Ticket Form

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,13 @@
+import CreateForm from "@/app/tickets/create/CreateForm";
+import React from "react";
+
+function CreateTicket(props) {
+    return (
+        <main>
+            <h2 className='text-primary text-center'>Add a new Ticket</h2>
+            <CreateForm />
+        </main>
+    );
+}
+
+export default CreateTicket;
